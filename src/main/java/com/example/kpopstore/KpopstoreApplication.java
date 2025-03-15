454a0b60d85a1a0791d7cd6ceb7f0c434e68c16d
TElzada <turdubekovaelzada@gmail.com>
2025-03-15 17:13:54 +0600

    Utilized Spring Initializr to create a new project

---
+++ src/main/java/com/example/kpopstore/KpopstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.kpopstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpopstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpopstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kpopstore/KpopstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kpopstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpopstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
