211136e3108774e93c3a9fc9d16c9dc991c631c8
TElzada <turdubekovaelzada@gmail.com>
2025-05-04 01:25:58 +0600

    Fixing

--- src/main/java/com/example/kpopstore/services/OrderItemService.java
+++ src/main/java/com/example/kpopstore/services/OrderItemService.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
-public class OrderItemService{
+public class OrderItemService  {
 
     private final OrderItemRepository orderItemRepository;
 
