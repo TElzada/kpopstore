d460f390c781d2dfc42d55f2f8c4fcad7d4fbc46
TElzada <turdubekovaelzada@gmail.com>
2025-05-05 14:23:59 +0600

    Starting with implementation of authentication using traditional login/password

---
+++ src/main/java/com/example/kpopstore/controllers/AuthController.java
@@ -0,0 +1,23 @@
+package com.example.kpopstore.controllers;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login"; // login.html в templates
+    }
+
+    @GetMapping("/profile")
+    public String profilePage(Model model, @AuthenticationPrincipal OAuth2User oauth2User) {
+        model.addAttribute("user", oauth2User);
+        return "profile";
+    }
+}
+
